f88ad019afd6b3b7b8dd2a8baf6e153bfa804114
tusharkumar04102001 <tusharkumar04102001@gmail.com>
2023-03-06 17:24:58 +0530

    created CustomerRepository.java

---
+++ src/main/java/com/niit/bej/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.niit.bej.repository;
+
+import com.niit.bej.domain.Customer;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends MongoRepository<Customer, Long> {
+}
