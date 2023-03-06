014f68426f9bfadd4c36280a768b9289bbe199fe
tusharkumar04102001 <tusharkumar04102001@gmail.com>
2023-03-06 18:48:42 +0530

    added register() in CustomerController.java

--- src/main/java/com/niit/bej/controller/CustomerController.java
+++ src/main/java/com/niit/bej/controller/CustomerController.java
@@ -1,7 +1,13 @@
 package com.niit.bej.controller;
 
+import com.niit.bej.domain.Customer;
+import com.niit.bej.exception.CustomerAlreadyExistException;
 import com.niit.bej.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,4 +18,17 @@ public class CustomerController {
     public CustomerController(CustomerService customerService) {
         this.customerService = customerService;
     }
+
+    @PostMapping("/customer/add")
+    public ResponseEntity<?> register(@RequestBody Customer customer) {
+        try {
+            Customer customer1 = customerService.register(customer);
+            return new ResponseEntity<>(customer1.getName() + "added to DataBase", HttpStatus.CREATED);
+        } catch (CustomerAlreadyExistException e) {
+            System.err.println(e.getMessage());
+            e.printStackTrace();
+            return new ResponseEntity<>("Customer Already Exist", HttpStatus.CONFLICT);
+        }
+    }
+
 }
--- src/main/java/com/niit/bej/exception/CustomerAlreadyExistException.java
+++ src/main/java/com/niit/bej/exception/CustomerAlreadyExistException.java
@@ -3,7 +3,7 @@ package com.niit.bej.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Customer is already exist in database")
+@ResponseStatus(code = HttpStatus.CONFLICT, reason = "Customer is already exist in database")
 public class CustomerAlreadyExistException extends Exception {
     public CustomerAlreadyExistException(String message) {
         super(message);
