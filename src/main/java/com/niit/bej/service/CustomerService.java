850ef96d489622bcf224722eafac315616ecef81
tusharkumar04102001 <tusharkumar04102001@gmail.com>
2023-03-06 17:38:52 +0530

    created CustomerServiceImpl.java

--- src/main/java/com/niit/bej/service/CustomerService.java
+++ src/main/java/com/niit/bej/service/CustomerService.java
@@ -1,9 +1,8 @@
 package com.niit.bej.service;
 
 import com.niit.bej.domain.Customer;
-import org.springframework.stereotype.Service;
 
-@Service
+
 public interface CustomerService {
     Customer login(String name, String password);
 }
---
+++ src/main/java/com/niit/bej/service/CustomerServiceImpl.java
@@ -0,0 +1,13 @@
+package com.niit.bej.service;
+
+import com.niit.bej.domain.Customer;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+    @Override
+    public Customer login(String name, String password) {
+        return null;
+    }
+}
