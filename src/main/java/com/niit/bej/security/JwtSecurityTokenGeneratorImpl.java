a4ca0b16b291a4ef0369e4259d84e669323a6db6
tusharkumar04102001 <tusharkumar04102001@gmail.com>
2023-03-06 19:32:54 +0530

    implemented generateToken() in JwtSecurityTokenGeneratorImpl.java

--- src/main/java/com/niit/bej/security/JwtSecurityTokenGeneratorImpl.java
+++ src/main/java/com/niit/bej/security/JwtSecurityTokenGeneratorImpl.java
@@ -1,7 +1,25 @@
 package com.niit.bej.security;
 
+import com.niit.bej.domain.Customer;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
+import java.util.Map;
+
 @Service
-public class JwtSecurityTokenGeneratorImpl {
+public class JwtSecurityTokenGeneratorImpl implements SecurityTokenGenerator {
+    @Override
+    public Map<String, String> generateToken(Customer customer) {
+        String generatedToken = Jwts.builder()
+                .setSubject(customer.getName())
+                .setIssuedAt(new Date())
+                .signWith(SignatureAlgorithm.HS256, "secretKey")
+                .compact();
+        return Map.of(
+                "token", generatedToken,
+                "message", "Token Generated and " + customer.getName() + " Logged in Successfully!"
+        );
+    }
 }
