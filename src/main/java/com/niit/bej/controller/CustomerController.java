7d9a414109b1d99b48ec97d896fb223459437709
tusharkumar04102001 <tusharkumar04102001@gmail.com>
2023-03-06 18:53:57 +0530

    added getAllCustomers() in CustomerController.java

--- src/main/java/com/niit/bej/controller/CustomerController.java
+++ src/main/java/com/niit/bej/controller/CustomerController.java
@@ -2,10 +2,12 @@ package com.niit.bej.controller;
 
 import com.niit.bej.domain.Customer;
 import com.niit.bej.exception.CustomerAlreadyExistException;
+import com.niit.bej.exception.CustomerNotFoundException;
 import com.niit.bej.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,6 +30,18 @@ public class CustomerController {
             System.err.println(e.getMessage());
             e.printStackTrace();
             return new ResponseEntity<>("Customer Already Exist", HttpStatus.CONFLICT);
+        }
+    }
+
+    @GetMapping("/customers")
+    public ResponseEntity<?> getAllCustomers() {
+        try {
+
+            return new ResponseEntity<>(customerService.getAllCustomer(), HttpStatus.FOUND);
+        } catch (CustomerNotFoundException e) {
+            System.err.println(e.getMessage());
+            e.printStackTrace();
+            return new ResponseEntity<>("Customer Not Found", HttpStatus.NOT_FOUND);
         }
     }
 
