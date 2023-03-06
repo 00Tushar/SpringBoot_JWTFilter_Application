7948d669b5a6b61f4f0392c3bb73e673efbe57f1
tusharkumar04102001 <tusharkumar04102001@gmail.com>
2023-03-06 18:39:46 +0530

    injected CustomerService in CustomerController.java

--- src/main/java/com/niit/bej/controller/CustomerController.java
+++ src/main/java/com/niit/bej/controller/CustomerController.java
@@ -1,7 +1,15 @@
 package com.niit.bej.controller;
 
+import com.niit.bej.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CustomerController {
+    private final CustomerService customerService;
+
+    @Autowired
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
 }
