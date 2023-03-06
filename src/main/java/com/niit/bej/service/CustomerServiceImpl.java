cffd5275c78e12b1844708afef9e252df5804b3a
tusharkumar04102001 <tusharkumar04102001@gmail.com>
2023-03-06 17:41:41 +0530

    injected CustomerRepository in CustomerServiceImpl.java

--- src/main/java/com/niit/bej/service/CustomerServiceImpl.java
+++ src/main/java/com/niit/bej/service/CustomerServiceImpl.java
@@ -1,10 +1,18 @@
 package com.niit.bej.service;
 
 import com.niit.bej.domain.Customer;
+import com.niit.bej.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CustomerServiceImpl implements CustomerService {
+    private final CustomerRepository customerRepository;
+
+    @Autowired
+    public CustomerServiceImpl(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
 
     @Override
     public Customer login(String name, String password) {
