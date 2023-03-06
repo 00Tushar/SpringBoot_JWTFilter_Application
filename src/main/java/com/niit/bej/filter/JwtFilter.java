26a8e1cab5984f29a22deaed6362e9285aee2795
tusharkumar04102001 <tusharkumar04102001@gmail.com>
2023-03-06 19:49:45 +0530

    added doFilter() in JwtFilter.java

--- src/main/java/com/niit/bej/filter/JwtFilter.java
+++ src/main/java/com/niit/bej/filter/JwtFilter.java
@@ -1,6 +1,37 @@
 package com.niit.bej.filter;
 
-public class JwtFilter {
+import io.jsonwebtoken.Jwts;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.GenericFilterBean;
+
+import java.io.IOException;
+
+public class JwtFilter extends GenericFilterBean {
 
 
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+
+        HttpServletResponse httpServletResponse = (HttpServletResponse) servletResponse;
+
+        ServletOutputStream outputStream = httpServletResponse.getOutputStream();
+
+        String authorizationHeader = httpServletRequest.getHeader("Authorization");
+
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) ;
+        {
+            String JwtToken = authorizationHeader.substring("Bearer ".length());
+            String username = Jwts.parser()
+                    .setSigningKey("secretKey")
+                    .parseClaimsJws(JwtToken)
+                    .getBody()
+                    .getSubject();
+            httpServletRequest.setAttribute("username", username);
+        }
+        filterChain.doFilter(servletRequest, servletResponse);
+
+    }
 }
