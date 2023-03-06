e127e2918523a4b37ce3e6117213e57b1af6d7dc
tusharkumar04102001 <tusharkumar04102001@gmail.com>
2023-03-06 17:19:09 +0530

    added fields for Customer.java

--- src/main/java/com/niit/bej/domain/Customer.java
+++ src/main/java/com/niit/bej/domain/Customer.java
@@ -1,7 +1,13 @@
 package com.niit.bej.domain;
 
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.MongoId;
 
 @Document
 public class Customer {
+    @MongoId
+    private long id;
+    private String name;
+    private String password;
+    private String phoneNo;
 }
