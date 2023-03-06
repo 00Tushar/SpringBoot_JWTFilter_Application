d189bfcf306122eedbbe9a26cf788e75b6251b5e
tusharkumar04102001 <tusharkumar04102001@gmail.com>
2023-03-06 17:20:02 +0530

    added getters and setters for Customer.java

--- src/main/java/com/niit/bej/domain/Customer.java
+++ src/main/java/com/niit/bej/domain/Customer.java
@@ -20,4 +20,36 @@ public class Customer {
 
     public Customer() {
     }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
 }
